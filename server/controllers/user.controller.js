1c6c5135708d8034b456c149fae9e0e4f82e3496
Adom Hartell <adomhartell@gmail.com>
2017-11-03 16:55:54 -0700

    add registerUser to user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,25 @@
 import User from '../models/user';
+import cuid from 'cuid';
+import sanitizeHtml from 'sanitize-html';
 
-export function getSomething(req, res) {
-  return res.status(200).end();
+
+export function registerUser(req, res) {
+  // TODO Subtler error handling, with better error messages
+  if (!req.body.post.name || !req.body.post.title || !req.body.post.content) {
+    res.status(403).end();
+  }
+
+  const input = req.body.user;
+  const email = sanitizeHtml(input.email);
+  const password = sanitizeHtml(input.password);
+
+  const user = new User({ email, password });
+  user.cuid = cuid();
+
+  user.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    return res.json({ user: saved });
+  });
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,6 @@ import * as UserController from '../controllers/user.controller';
 const router = new Router();
 
 // Sign up
-router.route('/users').post(UserController.addUser);
+router.route('/users').post(UserController.registerUser);
 
 export default router;
