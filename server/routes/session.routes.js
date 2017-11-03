4eb5dc45fdd1de4436ab82000a6554357d778039
Adom Hartell <adomhartell@gmail.com>
2017-11-03 15:57:30 -0700

    add routes for login/logout/signup

--- server/routes/session.routes.js
+++ server/routes/session.routes.js
@@ -3,5 +3,10 @@ import * as SessionController from '../controllers/session.controller';
 
 const router = new Router();
 
+// Login user
+router.route('/session').post(SessionController.login);
+
+// Logout user
+router.route('/session').delete(SessionController.logout);
 
 export default router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,5 +3,7 @@ import * as UserController from '../controllers/user.controller';
 
 const router = new Router();
 
+// Sign up
+router.route('/users').post(UserController.addUser);
 
 export default router;
